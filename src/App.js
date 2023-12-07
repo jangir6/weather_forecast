e4ecb42e9df28e33d9fa626b421a3f68e8ad92f2
jangir6 <jangir.6@iitj.ac.in>
2023-12-08 00:31:42 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import WeatherApp  from './Components/Weatherapp/WeatherApp.jsx';
+
+
+function App() {
+  return (
+    <div className="App">
+     <WeatherApp />
+    </div>
+  );
+}
+
+export default App;
